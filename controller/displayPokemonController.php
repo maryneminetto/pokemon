e9741d8e0cddcba8accdbe10682f3cc21b31a206
Maryne Minetto <maryne.minetto@live.fr>
2021-06-25 09:50:23 +0200

    [FIX] Load page

--- controller/displayPokemonController.php
+++ controller/displayPokemonController.php
@@ -5,11 +5,11 @@ include_once '../model/PokemonRepo.php';
 include_once 'session.php';
 
 $pokemon = new PokemonRepo();
-$pokemon->createPokemon();
 
 if(sizeof($_SESSION) == 0) {
     $pokemons = $pokemon->getArray();
     $_SESSION['pokedex'] = $pokemons;
+    $pokemon->createPokemon();
 }
 
 ?>
