71aeb9792c3f788822616b75ec8127d38b683df2
Maryne Minetto <maryne.minetto@live.fr>
2021-07-01 15:22:47 +0200

    [FEATURE] Add display Pokemon with DB

--- model/PokemonRepo.php
+++ model/PokemonRepo.php
@@ -89,7 +89,8 @@ class PokemonRepo
         return json_decode(file_get_contents($url), true);
     }
 
-    private function getPokemonFromJson($json): Pokemon {
+    private function getPokemonFromJson($json): Pokemon
+    {
         $name = $json['name'];
         $logo = $json['sprites']['other']['dream_world']['front_default'];
         $imageOffi = $json['sprites']['other']['official-artwork']['front_default'];
@@ -98,25 +99,77 @@ class PokemonRepo
         $id = $json['id'];
         $stats = [];
         $abilities = [];
+        $pdo = new PDO('mysql:host=localhost;dbname=exo_connection', 'root', '');
+        $queryPokemon = $pdo->prepare('SELECT * FROM PokemonList WHERE name = :name');
+        $queryPokemon->execute(array(':name' => $name));
+        $rowPokemon = $queryPokemon->fetch(PDO::FETCH_OBJ);
         foreach ($json['types'] as $type) {
-            $types[] = new Type($type['type']['name'], "../img/".$type['type']['name'].".png");
+            $queryType = $pdo->prepare('SELECT * FROM Type WHERE name = :name');
+            $queryType->execute(array(':name' => $type['type']['name']));
+            $row = $queryType->fetch(PDO::FETCH_OBJ);
+
+            if ($row == '') {
+                $queryInsertType = $pdo->prepare('INSERT INTO Type(name, imgPath) VALUES(:typeName, :imgPath)');
+                $queryInsertType->execute(array(':typeName' => $type['type']['name'], ':imgPath' => "../img/" . $type['type']['name'] . ".png"));
+            }
+
+            $queryHasType = $pdo->prepare('INSERT INTO hasType(id_pokemon, id_type) VALUES (:idPkmn, :idType)');
+            $queryHasType->execute(array(':idPkmn' => $rowPokemon->id, ':idType' => $row->id));
+
+
+            $types[] = new Type($type['type']['name'], "../img/" . $type['type']['name'] . ".png");
         }
-        foreach($json['stats'] as $stat) {
+
+        foreach ($json['stats'] as $stat) {
             $stats[$stat['stat']['name']] = new Stat($stat['stat']['name'], $stat['base_stat']);
             $stats[$stat['stat']['name']]->setColor($stats[$stat['stat']['name']]->getValue());
-
         }
-        foreach ($json['abilities'] as $ability){
+
+        $queryPokemon = $pdo->prepare('SELECT * FROM PokemonList WHERE name = :name');
+        $queryPokemon->execute(array(':name' => $name));
+        $row = $queryPokemon->fetch(PDO::FETCH_OBJ);
+        if ($row == '') {
+            $queryPokemon = $pdo->prepare('INSERT INTO PokemonList(name, pokedexNumber, pathLogo, pathImage, hp, attack, defense, specialAttack, specialDefense, speed) VALUES(:name, :pokedexNumber, :pathLogo, :pathImg, :hp, :attack, :defense, :specialAttack, :specialDefense, :speed)');
+
+            $queryPokemon->execute(array(':name' => $name,
+                    ':pokedexNumber' => $order,
+                    ':pathLogo' => $logo,
+                    ':pathImg' => $imageOffi,
+                    ':hp' => $stats['hp']->getValue(),
+                    ':attack' => $stats['attack']->getValue(),
+                    ':defense' => $stats['defense']->getValue(),
+                    ':specialAttack' => $stats['special-attack']->getValue(),
+                    ':specialDefense' => $stats['special-defense']->getValue(),
+                    ':speed' => $stats['speed']->getValue())
+            );
+        }
+
+
+        foreach ($json['abilities'] as $ability) {
             $ability = array_slice($ability, 0, 2);
             $abilityName = $ability['ability']['name'];
             $abilitiesEffect = $this->getAData($ability['ability']['url']);
             $abilityHidden = $ability['is_hidden'];
-            foreach($abilitiesEffect['effect_entries'] as $effect) {
-                if($effect['language']['name'] == 'en') {
+            foreach ($abilitiesEffect['effect_entries'] as $effect) {
+                if ($effect['language']['name'] == 'en') {
+
+                    $queryType = $pdo->prepare('SELECT * FROM Ability WHERE name = :name');
+                    $queryType->execute(array(':name' => $abilityName));
+                    $row = $queryType->fetch(PDO::FETCH_OBJ);
+
+                    if ($row == '') {
+                        $queryInsertAbility = $pdo->prepare('INSERT INTO Ability(name, effect, is_hidden) VALUES(:abilityName, :effect, :is_hidden)');
+                        $queryInsertAbility->execute(array(
+                            ':abilityName' => $abilityName,
+                            ':effect' => $effect['effect'],
+                            ':is_hidden' => $abilityHidden));
+                    }
+                    $queryHasAbility = $pdo->prepare('INSERT INTO hasAbility(id_pokemon, id_ability) VALUES (:idPkmn, :idAbility)');
+                    $queryHasAbility->execute(array(':idPkmn' => $rowPokemon->id, ':idAbility' => $row->id));
+
                     $abilities[$abilityName] = new Ability($abilityName, $effect['effect'], $abilityHidden);
                 }
             }
-
         }
         return new Pokemon($name, $logo, $imageOffi, $order, $types, $id, $stats, $abilities);
     }
--- view/displayPokemon.php
+++ view/displayPokemon.php
@@ -2,35 +2,40 @@
 
 include_once '../controller/displayPokemonController.php';
 
-
-
-$pokemonsArray = $_SESSION['pokedex'];
-
+$name = "";
 
 foreach($pokemonsArray as $poke=>$info){
+
+    if ($info->Type1 == "" || $name == $info->name){
+
+        continue;
+
+    }else {
+
+    $name = $info->name;
+
     ?>
     <div class='flex flex-col justify-center items-center border-8 border-blue-900 p-4 m-4 rounded-lg'>
-        <img src="<?php echo $info->getImg() ?>" class="w-72 max-h-72">
+        <img src="<?php echo $info->pathLogo ?>" class="w-72 max-h-72">
         <div class="content flex flex-col justify-center items-center text-gray-900 my-auto p-4">
             <div class="title flex justify-center items-center mb-5">
-                <p class="text-xl text-center font-semibold uppercase border border-gray-900 py-2 px-4 rounded-full"> # <?php echo $info->getNumber() ?> </p>
-                <h2 class=" text-xl text-center font-semibold uppercase ml-5"><?php echo $info->getName() ?></h2>
+                <p class="text-xl text-center font-semibold uppercase border border-gray-900 py-2 px-4 rounded-full"> # <?php echo $info->pokedexNumber ?> </p>
+                <h2 class=" text-xl text-center font-semibold uppercase ml-5"><?php echo $info->name ?></h2>
             </div>
             <div class="type flex justify-center items-center">
-                <?php
 
-                foreach ($info->getType() as $type){
-                    ?>
-                    <img src="<?php echo $type->getImg() ?>" alt="" width="100px" class="mx-4">
-                    <?php
-                }
-                ?>
+                <img src="../img/<?php echo $info->Type2; ?>.png" alt="" width="100px">
+                <img src="../img/<?php echo $info->Type1; ?>.png" alt="" width="100px">
+
+
             </div>
 
-            <a href="displayDetail.php?url=https://pokeapi.co/api/v2/pokemon/<?php echo $info->getId() ?>/" class="btn-blue px-4 py-2 mt-6 w-40 font-semibold text-center">Voir la fiche</a>
+            <a href="displayDetail.php?url=https://pokeapi.co/api/v2/pokemon/<?php echo $info->id ?>/" class="btn-blue px-4 py-2 mt-6 w-40 font-semibold text-center">Voir la fiche</a>
         </div>
     </div>
     <?php
+    }
+
 }
 
 ?>
